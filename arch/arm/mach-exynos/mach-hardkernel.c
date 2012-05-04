4d6a710d4b46f1f9e80fa91dfed72ace791516a3
Dongjin Kim <dongjin.kim@agreeyamobility.net>
2012-05-04 14:13:51 +0900

    HARDKERNEL: MAX8997 connections to Exynos4
    IRQ1B -> GPX0(0)
    SET1  -> GPX1(6)
    SET2  -> GPX1(7)
    SET3  -> GPX0(4)

--- arch/arm/mach-exynos/mach-hardkernel.c
+++ arch/arm/mach-exynos/mach-hardkernel.c
@@ -432,9 +432,9 @@ static struct max8997_platform_data __initdata hardkernel_max8997_pdata = {
 	.ignore_gpiodvs_side_effect = true,
 	.buck125_default_idx = 0x0,
 
-	.buck125_gpios[0]	= EXYNOS4_GPX0(0),
-	.buck125_gpios[1]	= EXYNOS4_GPX0(1),
-	.buck125_gpios[2]	= EXYNOS4_GPX0(2),
+	.buck125_gpios[0]	= EXYNOS4_GPX1(6),
+	.buck125_gpios[1]	= EXYNOS4_GPX1(7),
+	.buck125_gpios[2]	= EXYNOS4_GPX0(4),
 
 	.buck1_voltage[0]	= 1350000,
 	.buck1_voltage[1]	= 1300000,
@@ -469,7 +469,7 @@ static struct i2c_board_info i2c0_devs[] __initdata = {
 	{
 		I2C_BOARD_INFO("max8997", (0xCC >> 1)),
 		.platform_data	= &hardkernel_max8997_pdata,
-		.irq		= IRQ_EINT(4),
+		.irq		= IRQ_EINT(0),
 	},
 };
 
@@ -819,9 +819,9 @@ static void __init hardkernel_map_io(void)
 
 static void __init hardkernel_power_init(void)
 {
-	gpio_request(EXYNOS4_GPX0(4), "PMIC_IRQ");
-	s3c_gpio_cfgpin(EXYNOS4_GPX0(4), S3C_GPIO_SFN(0xf));
-	s3c_gpio_setpull(EXYNOS4_GPX0(4), S3C_GPIO_PULL_NONE);
+	gpio_request(EXYNOS4_GPX0(0), "PMIC_IRQ");
+	s3c_gpio_cfgpin(EXYNOS4_GPX0(0), S3C_GPIO_SFN(0xf));
+	s3c_gpio_setpull(EXYNOS4_GPX0(0), S3C_GPIO_PULL_NONE);
 
 	// system power enable (P3V3)
 	gpio_request(EXYNOS4_GPX1(1), "p3v3_en");
